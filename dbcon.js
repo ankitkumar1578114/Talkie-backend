c82fdab96357fd1d19369c0b730c9d512dbc4e8d
Ankit Kumar <ankitkumar1578114@gmail.com>
2023-10-04 11:43:27 +0530

    some changes

--- dbcon.js
+++ dbcon.js
@@ -2,10 +2,16 @@ const mysql = require('mysql')
 
 const conn = mysql.createConnection(
     {
-        host:"localhost",        
-        user:'root',
-        password:"",
-        database:'movie_dataset'
+        host:"bud7smgco12prwhvcdqt-mysql.services.clever-cloud.com",        
+        user:'ug4opd26a5o0wbnr',
+        password:"A8Uqhger5xSXWCreeKM6",
+        database:'bud7smgco12prwhvcdqt',
+        port: 3306
+        // host:"localhost",        
+        // user:'root',
+        // password:"",
+        // database:'movie',
+
     }
 
 )
